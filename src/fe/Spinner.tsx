e07b3df393b27a5d774ed4a999e2d1e3214c1556
mpaulweeks <mpaulweeks@gmail.com>
2022-03-14 14:24:24 -0400

    add spinner img and code

---
+++ src/fe/Spinner.tsx
@@ -0,0 +1,17 @@
+
+export function Spinner(props: {
+  height?: string;
+  width?: string;
+}) {
+  const height = props.height ?? 'auto';
+  const width = props.width ?? 'auto';
+  return (
+    <img
+      style={{
+        height,
+        width,
+      }}
+      src='images/spinner.gif'
+    />
+  );
+}
