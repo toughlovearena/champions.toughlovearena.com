a6cdf2d111eef2866d073ddc925eee3eee302a90
mpaulweeks <mpaulweeks@gmail.com>
2022-03-22 16:00:24 -0400

    pretty the rest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
 };
--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 The official website for the Tough Love Arena Hall of Fame
 
 Want to submit your event to the Hall of Fame? Send us an [email](mailto:toughlovearena@gmail.com) or message us on [Twitter](https://twitter.com/ToughLoveArena)
-
